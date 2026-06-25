c4eab280fe9bff23b383290d0e50a79244a4dee9
crpgproductions <crpgproductions@hotmail.de>
2026-06-25 13:21:51 +0200

    fix: logo font issue on Firefox | style: Make light mode background slightly more transparent

--- app/components/PageSkeleton.tsx
+++ app/components/PageSkeleton.tsx
@@ -5,7 +5,7 @@ import Footer from "@/app/components/Footer";
 export default function PageSkeleton({ children }: { children: React.ReactNode }) {
   return (
     <AuroraBackground className="flex flex-col flex-1 items-center justify-center font-sans min-h-screen">
-      <main className="relative flex flex-1 w-full max-w-7xl flex-col items-center pt-32 px-16 sm:items-start bg-white/80 dark:bg-black/60
+      <main className="relative flex flex-1 w-full max-w-7xl flex-col items-center pt-32 px-16 sm:items-start bg-white/70 dark:bg-black/60
 before:absolute before:inset-0 before:backdrop-blur-sm before:-z-10">
         <Menu />
         {children}
